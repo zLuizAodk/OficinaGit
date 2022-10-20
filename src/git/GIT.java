7595cce0a2650f6f954fb48c82b88dcbfeb5ffe1
AODK <luizfernandoalvescorreia@gmail.com>
2022-10-19 20:50:15 -0400

    atividades

---
+++ src/git/GIT.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git;
+
+/**
+ *
+ * @author teclab
+ */
+public class GIT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nosso primeiro projetin né felas de gerenciamento de versão");
+        System.out.println("Alterando...");
+       
+    }
+    
+}
